8a3ea58f24c95b2fd077e06a4d2aa2100e820bab
David Goodman <null>
2014-01-13 16:03:43 -0800

    played around with scope per instructions

--- sneaks.rb
+++ sneaks.rb
@@ -1,26 +1,73 @@
-shoes = "sneakers"
+# local_var = "Sneakers"
+# def get_local_var
+#   local_var = "suhsi"
+#   local_var
+# end
 
-def show(this)
-  p this #methods cannot use a variable not defined within the method or passed to it.  Cannot just p shoes or name
+# p get_local_var
+
+THIS_IS_A_CONSTANT = "A$AP Rocky"
+$global_var = "DANGER DOOM"
+
+def get_constant
+   THIS_IS_A_CONSTANT
 end
 
-show(shoes)
+def get_global_var
+  $global_var
+end
+
+def set_global_var=(this)
+  $global_var = this
+end
+
+p get_global_var
+p set_global_var = "Talib"
+
+# def set_constant=(this)
+#    THIS_IS_A_CONSTANT = this
+# end
+
+#p get_constant
+p $global_var
+
+# class BasicClass
+#   attr_reader :local_var, :instance_var
+#   def initialize(thing)
+#     @instance_var = thing
+#     local_var = "Sneakers"
+#     @@class_var = "Danny Brown"
+#   end
+
+#   def get_class_var
+#     @@class_var
+#   end
+
+#   def set_class_var=(new_value)
+#     @@class_var = new_value
+#   end
 
 
 
-
-# class Sneakers
-#   attr_reader :brand
-#  def initialize(brand)
-#   @brand = brand
+#  def get_local_var
+#   # local_var = "suhsi"
+#   local_var
 #  end
 
-#  def show_brand
-#    "Nice shoes, are they " + @brand
+#  def set_instance_var=(new_value)
+#     @instance_var = new_value
+#  end
+
+#  def get_instance_var
+#   @instance_var
 #  end
 # end
 
-# nikes = Sneakers.new("Nikes")
-# vans = Sneakers.new("Vans")
-# p nikes.show_brand == "Nice shoes, are they Nikes"
-# p vans.show_brand == "Nice shoes, are they Vans"
+# name = BasicClass.new("Sneakers")
+# p name.set_instance_var = "eraser"
+# p name.instance_var
+# p name.get_class_var
+# p name.set_class_var = "Riff Raff"
+# p name.get_class_var
+# jump = BasicClass.new("Action Bronson")
+# p jump.get_class_var
